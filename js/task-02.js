63ce7834bb9884d61ce471317271f6fdf4154201
YuriyOhii <uaogiy@gmail.com>
2023-05-02 21:48:34 +0300

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ulRef = document.getElementById("ingredients");
+
+const ingregientsArrayRef = ingredients.map(el => {
+  const ref = document.createElement('li');
+  ref.textContent = el;
+  ref.classList.add('item');
+  return ref;
+});
+
+ulRef.append(...ingregientsArrayRef);
