e2525dab2d5c410d05d46483a07781154ad4ac8c
YuriyOhii <uaogiy@gmail.com>
2023-05-06 02:49:43 +0300

    eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const {elements:{email, password}} = evt.currentTarget;
+
+    if (email.value === '' || password.value === '') alert('Всі поля мають бути заповнені!!');
+    const formData = {
+        [email.name]:email.value,
+        [password.name]:password.value,
+    };
+    console.table(formData);
+    evt.currentTarget.reset();
+    }
