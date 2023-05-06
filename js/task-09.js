476936e5188f42256a951d32c3a0e1353962b01d
YuriyOhii <uaogiy@gmail.com>
2023-05-06 19:19:36 +0300

    ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,24 @@
+const refs = {
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+};
+
+refs.button.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  const colorVar = getRandomHexColor();
+  refs.span.textContent = colorVar;
+  document.body.style.backgroundColor = `${colorVar}`
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
+
+
